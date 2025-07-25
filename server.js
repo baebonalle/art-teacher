4f4a3263e2be5f06e38bfb95186c234f1ce8ab8a
baebonalle <bae.bonalle@gmail.com>
2025-07-25 17:11:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -17,12 +17,12 @@
 // })
 
 const express = require('express');
-// const cors = require('cors');
+const cors = require('cors');
 const path = require('path');
 const { spawn } = require('child_process');
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'pug');
