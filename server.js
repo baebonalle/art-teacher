96a037a1c904146164a4d752f551dab1fb209069
baebonalle <bae.bonalle@gmail.com>
2025-08-07 14:10:04 -0400

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { spawn } = require('child_process');
 const app = express();
 
+app.use(express.json({ limit: '50mb' }));
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
@@ -77,4 +78,5 @@ app.post('/mistralapi', (req, res) => {
 const server = app.listen(7000, () => {
   console.log(`Express running → PORT ${server.address().port}`);
 });
+
 
